5b17ab1c15b31702b8580eb6c5e07cb4ad3a22b6
Aravind <aravindds0552@gmail.com>
2025-06-24 10:26:03 +0530

    Editing ADD button in gui

--- .venv/Scripts/GUI.py
+++ .venv/Scripts/GUI.py
@@ -2,10 +2,24 @@ import functions
 import FreeSimpleGUI
 
 label = FreeSimpleGUI.Text("Type to-do in app")
-input_box = FreeSimpleGUI.InputText(tooltip="enter todo")
+input_box = FreeSimpleGUI.InputText(tooltip="enter todo",key="todo")
 add_button = FreeSimpleGUI.Button("Add")
-label1 = FreeSimpleGUI.Text("Type to-do today")
-input_box1 = FreeSimpleGUI.InputText(tooltip="enter todo")
-window = FreeSimpleGUI.Window("My To-Do App",layout=[[label],[input_box,add_button],[label1,input_box1]])
-window.read()
+#label1 = FreeSimpleGUI.Text("Type to-do today")
+#input_box1 = FreeSimpleGUI.InputText(tooltip="enter todo")
+window = FreeSimpleGUI.Window("My To-Do App",
+                              layout=[[label],[input_box,add_button]])
+while True:
+      event,values = window.read()
+      print(event)
+      print(values)
+      match event:
+          case "Add":
+              todos = functions.get_todos("todos.txt")
+              new_todo = values['todo'] + '\n'
+              todos.append(new_todo)
+              functions.write_todos("todos.txt",todos)
+              #print(event)
+              #print(values)
+          case FreeSimpleGUI.WINDOW_CLOSED:
+              break
 window.close()
