a4d6116689fbbfc752d77d0dbfb874da43bde4a5
Aravind <aravindds0552@gmail.com>
2025-06-24 20:20:44 +0530

    Implement EDIT,COMPLETE buttons

--- .venv/Scripts/GUI.py
+++ .venv/Scripts/GUI.py
@@ -6,8 +6,16 @@ input_box = FreeSimpleGUI.InputText(tooltip="enter todo",key="todo")
 add_button = FreeSimpleGUI.Button("Add")
 #label1 = FreeSimpleGUI.Text("Type to-do today")
 #input_box1 = FreeSimpleGUI.InputText(tooltip="enter todo")
+list_box = FreeSimpleGUI.Listbox(values=functions.get_todos("todos.txt"),enable_events=True,size =[45,10],key="todos")
+edit_button = FreeSimpleGUI.Button("Edit")
+complete_button = FreeSimpleGUI.Button("complete")
+exit_button = FreeSimpleGUI.Button("Exit")
 window = FreeSimpleGUI.Window("My To-Do App",
-                              layout=[[label],[input_box,add_button]])
+                              layout=[[label],[input_box,add_button],
+                                      [list_box,edit_button,complete_button],
+                                      [exit_button]],)
+
+
 while True:
       event,values = window.read()
       print(event)
@@ -18,8 +26,31 @@ while True:
               new_todo = values['todo'] + '\n'
               todos.append(new_todo)
               functions.write_todos("todos.txt",todos)
+              window['todos'].update(values=todos)
               #print(event)
               #print(values)
+          case "Edit":
+              todos = functions.get_todos("todos.txt")
+              todo_to_edit = values['todos'][0]
+              new_todo = values['todo']+ '\n'
+              n = todos.index(todo_to_edit)
+              todos[n] = new_todo
+              functions.write_todos("todos.txt",todos)
+              window['todos'].update(values=todos)
+          case "todos":
+               window['todo'].update(value=values['todos'][0])
+          case "complete":
+               todos = functions.get_todos("todos.txt")
+               todo_to_complete = values['todos'][0]
+               #todos.remove(todo_to_complete)
+               n = todos.index(todo_to_complete)
+               todos.pop(n)
+               functions.write_todos("todos.txt",todos)
+               window['todos'].update(values=todos)
+               window['todo'].update(value=" ")
+          case "Exit":
+              break
+
           case FreeSimpleGUI.WINDOW_CLOSED:
               break
 window.close()
